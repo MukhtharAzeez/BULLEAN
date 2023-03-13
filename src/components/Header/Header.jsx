65ef8d5decf5570d0e6d6f1e5bda195980272b17
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-03-13 16:51:33 +0530

    Header and sideBar completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,252 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect, useContext } from "react";
+import logo from "../../images/Bullean.png";
+import { Link } from "react-router-dom";
+import { darkThemeContext } from "../../context/darkTheme";
+import threeLine from "../../images/threeLine.png";
+import './Header.css'
+import SideBar from "./SideBar";
+
+function Header({ toggle, setToggle }) {
+  const [raiscap, setRaiscap] = useState(false);
+  const [openSideBar, setOpenSideBar] = useState(false)
+  const { theme, setTheme } = useContext(darkThemeContext);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <div className="w-full fixed z-50">
+      <div className="absolute w-[95vw] z-20 rounded-lg mx-4 p-4 sm:px-4 bg-[#efefef] dark:bg-[#10194D] text-[10px] sm:text-[16px] flex justify-between items-center">
+        <img
+          className="w-[90px] sm:w-fit pl-8"
+          src={logo}
+          onClick={handleTheme}
+          alt=""
+        />
+        <Link to="/" className="hideHeaderTitles">
+          <p className="cursor-pointer dark:text-white text-black">
+            Curated for you
+          </p>
+        </Link>
+        <Link to="/service" className="hideHeaderTitles">
+          <p className=" dark:text-white text-black cursor-pointer">Services</p>
+        </Link>
+        <p
+          onClick={() => setRaiscap(true)}
+          className="cursor-pointer dark:text-white text-black hideHeaderTitles"
+        >
+          Raise Capital / Sell Business
+        </p>
+        <Link to="/investor" className="hideHeaderTitles">
+          <p className="cursor-pointer dark:text-white text-black ">
+            Interested in investing
+          </p>
+        </Link>
+        <Link to="/support" className="hideHeaderTitles">
+          <p className="pr-8 cursor-pointer dark:text-white text-black ">
+            Contact Us
+          </p>
+        </Link>
+
+        {!openSideBar && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 dark:text-white text-black showBarIcon"
+            onClick={() => setOpenSideBar(true)}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        )}
+
+        {openSideBar && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 dark:text-white text-black showBarIcon"
+            onClick={() => setOpenSideBar(false)}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        )}
+      </div>
+
+      {toggle ? (
+        <div className="absolute top-0 backdrop-blur-sm z-20 w-full h-[100vh] mb-12 flex justify-center items-center">
+          <div className="flex flex-col items-center p-6 bg-white dark:bg-slate-800 rounded-xl">
+            <p className="text-[26px] ranade mb-8  dark:text-white text-black">
+              Subscription
+            </p>
+            <div className="flex justify-evenly items-center">
+              <div>
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Company name
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Concerned Person
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Designation
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Phone
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Email
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+              </div>
+              <div>
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Industry
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Sub Industry
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Sub Sub Industry
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Description
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+                <p className="mx-6 text-[13px]  dark:text-white text-black">
+                  Revenue Range / Arr
+                </p>
+                <input
+                  className="bg-[#c1bfbf] dark:bg-white text-black px-3 py-[6px] w-[472px] mx-6 mb-3 rounded-md"
+                  type="text"
+                />
+              </div>
+            </div>
+            <button
+              onClick={() => setToggle(false)}
+              className="my-3 text-[13px] border rounded-2xl px-10 py-2 bg-gradient-to-r from-green-400 to-blue-700"
+            >
+              Subscribe Now
+            </button>
+          </div>
+        </div>
+      ) : null}
+
+      {raiscap ? (
+        <div className="absolute top-0 backdrop-blur-sm z-20 w-full h-[100vh] mb-12 flex justify-center items-center">
+          <div className="flex flex-col items-center p-6 bg-white dark:bg-slate-800 rounded-xl">
+            <p className="text-[26px] ranade mb-8 text-black dark:text-white">
+              Raise Capital / Sell Business
+            </p>
+            <div className="text-[16px]">
+              <div className="flex justify-between items-center">
+                <div className="mx-6">
+                  <p className="text-black dark:text-white">First Name</p>
+                  <input
+                    className="px-3  bg-[#c1bfbf] dark:bg-white text-black py-[6px] rounded-lg"
+                    type="text"
+                  />
+                </div>
+                <div className="mx-6">
+                  <p className="text-black dark:text-white">Last Name</p>
+                  <input
+                    className="px-3 text-black py-[6px] rounded-lg bg-[#c1bfbf] dark:bg-white"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <p className="mt-6 mx-6 text-black dark:text-white">
+                Company Name
+              </p>
+              <input
+                className="px-3 text-black py-[6px] w-[550px] mx-6   rounded-lg bg-[#c1bfbf] dark:bg-white"
+                type={"text"}
+              />
+              <br />
+              <p className="mt-6 mx-6 text-black dark:text-white">
+                Email Address
+              </p>
+              <input
+                className="px-3 text-black py-[6px] w-[550px] mx-6 rounded-lg bg-[#c1bfbf] dark:bg-white"
+                type={"text"}
+              />
+              <br />
+              <p className="mt-6 mx-6 text-black dark:text-white">Password</p>
+              <input
+                className="bg-[#c1bfbf] dark:bg-white px-3 text-black py-[6px] w-[550px] mx-6 rounded-lg"
+                type={"text"}
+              />
+              <br />
+            </div>
+            <button
+              onClick={() => setRaiscap(false)}
+              className="my-3 mt-6 text-[13px] border rounded-2xl px-10 py-2 bg-gradient-to-r from-green-400 to-blue-700"
+            >
+              Subscribe Now
+            </button>
+          </div>
+        </div>
+      ) : null}
+
+      {openSideBar && <SideBar />}
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/SideBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./SideBar.css";
+import { Link } from "react-router-dom";
+
+function SideBar() {
+  return (
+    <div className="sidebar hideSideBar fixed top-6 bottom-0 right-0 p-2 w-[300px] overflow-y-auto bg-[#efefef] dark:bg-[#10194D]">
+      <div className="p-2.5 mt-16 flex flex-col rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-black dark:text-white">
+        <Link to="/">
+          <span className="text-[15px] ml-4 font-bold">Home</span>
+        </Link>
+
+        <Link to="/service" className="pt-8">
+          <span className="text-[15px] ml-4 font-bold">Curated for you</span>
+        </Link>
+
+        <Link to="/service" className="pt-8">
+          <span className="text-[15px] ml-4 font-bold">Services</span>
+        </Link>
+
+        <Link to="/service" className="pt-8">
+          <span className="text-[15px] ml-4 font-bold">
+            Raise Capital / Sell Business
+          </span>
+        </Link>
+
+        <Link to="/service" className="pt-8">
+          <span className="text-[15px] ml-4 font-bold">
+            Interested in investing
+          </span>
+        </Link>
+
+        <Link to="/service" className="pt-8">
+          <span className="text-[15px] ml-4 font-bold">Contact Us</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
